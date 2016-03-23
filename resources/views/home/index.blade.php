d8d7b9152d0840e8ee5c95b7839752d63356e898
chenxiang <851590535@qq.com>
2016-03-23 23:02:31 +0800

    基础布局

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>在线小工具</title>
+
+        
+        <link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="/css/base.css" rel="stylesheet">
+        <script src="/js/jquery-1.12.2.min.js"></script>
+        <script src="/bootstrap/js/bootstrap.min.js"></script>
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="navbar navbar-fixed-top topaddress">
+        <div class="container" style="width: 100%;">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-1">
+                            <span class="glyphicon glyphicon-arrow-left goback"></span>
+                            <span class="glyphicon glyphicon-arrow-right goback"></span>
+                            <span class="glyphicon glyphicon-repeat goback-black"></span>
+                            <span class="glyphicon glyphicon-home goback-black"></span>
+                        </div>
+                        <div class="col-md-11">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="uqian.me" aria-describedby="basic-addon2">
+                                <span class="input-group-addon glyphicon glyphicon-th-list" id="basic-addon2"></span>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    111
+                </div>
+            </div>
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>
+        </div>
+    </body>
+</html>
